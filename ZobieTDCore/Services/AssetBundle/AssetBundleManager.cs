b91f6246c6b5285e79a02efc9897bd09674d7307
TrdHuy <trdtranduchuy@gmail.com>
2025-04-17 20:18:22 +0700

    Init project

---
+++ ZobieTDCore/Services/AssetBundle/AssetBundleManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZobieTDCore.Services.AssetBundle
+{
+    public class AssetBundleManager
+    {
+        public static string GetMessage() => "Xin chào từ .NET Standard!";
+    }
+}
