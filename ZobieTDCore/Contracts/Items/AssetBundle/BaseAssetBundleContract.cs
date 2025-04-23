078e3a92e98e54d144dac5fbfecb573623ebf5d3
huy.td1 <huy.td1@samsung.com>
2025-04-23 18:29:11 +0700

    Cache all asset to single cache when load all asset Release softly bundle when load all in cache

---
+++ ZobieTDCore/Contracts/Items/AssetBundle/BaseAssetBundleContract.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZobieTDCore.Contracts.Items.AssetBundle
+{
+    public abstract class BaseAssetBundleContract : IAssetBundleContract
+    {
+        private bool isUnloaded = false;
+        private bool isSoftUnloaded = false;
+
+
+        public abstract string BundleName { get; }
+
+        public abstract bool Contain(object asset);
+
+        public abstract T[] LoadAllSubAssets<T>() where T : class;
+
+        public abstract T LoadSingleSubAsset<T>(string name) where T : class;
+
+        public void Unload(bool unloadAllAsset)
+        {
+            if (isUnloaded)
+            {
+                return;
+            }
+            isUnloaded = true;
+            isSoftUnloaded = !unloadAllAsset;
+
+            UnloadInternal(unloadAllAsset);
+        }
+
+        protected abstract void UnloadInternal(bool unloadAllAsset);
+    }
+}
